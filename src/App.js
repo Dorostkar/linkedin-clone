a141e0f68800eb458a787f517ebf120fd594b2c4
Sina.Dorostkar@chrysalisloyalty.com <Sina.Dorostkar@chrysalisloyalty.com>
2021-02-12 16:23:39 +0000

    Header, HeaderOption, Avatar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
+
 import './App.css';
+import Header from './Header';
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Lets build a linkedin Clone</h1>
+		<div className="app">
+			<Header />
+			{/* App Body */}
+			{/* Sidebar */}
+			{/* Feed */}
+			{/* Widgets */}
 		</div>
 	);
 }
